29dd54881dadb0e5628e8cca7fbef3698eae3849
Staszek Cieslicki <s.cieslicki@clearcode.cc>
2020-04-10 15:52:14 +0200

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "eu-central-1",
+    BUCKET: "met-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "eu-central-1",
+    URL: "https://tkbf48z5t8.execute-api.eu-central-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "eu-central-1",
+    USER_POOL_ID: "eu-central-1_NDHry3ikl",
+    APP_CLIENT_ID: "64fc8i4ak0u63tct8m99iu0i7t",
+    IDENTITY_POOL_ID: "eu-central-1:9d8a1b73-75ca-4cfc-b4cc-268b915606e4"
+  }
+};
